095b2cc88e6ccbab98006615774d24b7a163058e
Mostafa Hirsi <mostafahirsi@Mostafas-MBP.cust.communityfibre.co.uk>
2023-03-05 13:12:20 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { VechaiProvider, Button } from "@vechaiui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-wrap w-full p-8 space-x-2">
+      <Button color="primary">Button</Button>
+      <Button variant="solid" color="primary">
+        Button
+      </Button>
+      <Button variant="light" color="primary">
+        Button
+      </Button>
+      <Button variant="ghost" color="primary">
+        Button
+      </Button>
+      <Button variant="link" color="primary">
+        Button
+      </Button>
     </div>
   );
 }
